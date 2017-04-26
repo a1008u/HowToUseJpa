51fa0258975f49d373c12ba6829bd1acb6a44d7a
a1008u <empty.aauu0001@gmai.com>
2017-04-26 22:23:20 +0900

    first_commit

---
+++ HowToUseJpa/src/main/java/domain/MyData.java
@@ -0,0 +1,33 @@
+package domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "mydata")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyData {
+	
+	@Id
+    @GeneratedValue
+    private int id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column
+    private int age;
+    
+    @Column
+    private String message;
+
+}
